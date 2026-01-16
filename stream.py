a7bdb453d27296095e0b46711a69eb07d1b11d4f
ispeaknerd <96841384+iSpeakNerd@users.noreply.github.com>
2026-01-16 12:00:22 -0800

    describe AsyncGenerator pattern

--- stream.py
+++ stream.py
@@ -8,6 +8,15 @@ settings = Settings()
 
 app = FastAPI(title="stream ex")
 
+# Once a generator, always a generator until final usage
+# pattern is
+# async def api_response():
+#   step1 = source_generator()
+#   step2 = logic(step1)
+#   .....
+#   final_step = last_logic(penultimate_step)
+#   return StreamingResponse(final_step)
+
 
 async def stream_generator() -> AsyncGenerator:
     array_ex = ["some", "strings", "are", "longer than others", "."]
@@ -17,6 +26,8 @@ async def stream_generator() -> AsyncGenerator:
 
 async def slow_logic(gen: AsyncGenerator) -> AsyncGenerator:
     from time import sleep
+
+    # display each chunk as we go instead of waiting for whole return
     import asyncio
 
     async for i in gen:
