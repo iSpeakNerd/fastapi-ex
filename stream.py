7988b16829f138a5f28c10a4d398dc2fc5531920
ispeaknerd <96841384+iSpeakNerd@users.noreply.github.com>
2026-01-16 11:45:55 -0800

    stream response

---
+++ stream.py
@@ -0,0 +1,41 @@
+from random import random
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse, StreamingResponse
+from config import Settings
+
+settings = Settings()
+
+app = FastAPI(title="stream ex")
+
+
+async def stream_generator():
+    array_ex = ["some", "strings", "are", "longer than others", "."]
+    for i in array_ex:
+        yield i
+
+
+async def slow_logic(gen):
+    from time import sleep
+    import asyncio
+
+    async for i in gen:
+        await asyncio.sleep(random() * 4)
+        yield i + " "
+
+
+@app.api_route("/stream", methods=["GET"])
+async def stream_ex():
+    gen = stream_generator()
+    modded = slow_logic(gen)
+    return StreamingResponse(modded)
+
+
+@app.get("/")
+def home():
+    return JSONResponse("hello")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("stream:app", reload=True)
